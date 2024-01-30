2f9809942b2f9a8c58acebfd985f69e0aa9bbd31
gian maria ricci <ricci.gianmaria@gmail.com>
2024-01-30 22:21:44 +0100

    Sample initial

---
+++ src/KernelMemory.AtlasMongoDb.Tests/AtlasMemory.cs
@@ -0,0 +1,161 @@
+﻿using System.Collections.Immutable;
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+using Microsoft.KernelMemory;
+using Microsoft.KernelMemory.MemoryStorage;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace KernelMemory.AtlasMongoDb.Tests;
+
+public class AtlasMemory : IMemoryDb
+{
+    private readonly MongoClient _client;
+    private readonly IMongoDatabase _db;
+    private readonly AtlasSearchUtils _utils;
+
+    private const string ConnectionNamePrefix = "_ix_";
+
+    public AtlasMemory(string connectionString, string databaseName)
+    {
+        _client = new MongoClient(connectionString);
+        _db = _client.GetDatabase(databaseName);
+        _utils = new AtlasSearchUtils(connectionString, databaseName);
+    }
+
+    private IMongoCollection<BsonDocument> GetCollection(string indexName)
+    {
+        var collectionName = GetCollectionName(indexName);
+        return _db.GetCollection<BsonDocument>(collectionName);
+    }
+
+    private static string GetCollectionName(string indexName)
+    {
+        return $"{ConnectionNamePrefix}{indexName}";
+    }
+
+    public async Task CreateIndexAsync(string index, int vectorSize, CancellationToken cancellationToken = default)
+    {
+        //Index name is the name of the collection, so we need to understand if the collection exists
+        var collectionName = GetCollectionName(index);
+        await _utils.CreateIndexAsync(collectionName, vectorSize);
+        await _utils.WaitForIndexToBeReady(collectionName, 60);
+    }
+
+    public async Task DeleteIndexAsync(string index, CancellationToken cancellationToken = default)
+    {
+        var collectionName = GetCollectionName(index);
+        await _utils.DeletedIndicesAsync(collectionName);
+        await _db.DropCollectionAsync(collectionName);
+    }
+
+    public async Task<IEnumerable<string>> GetIndexesAsync(CancellationToken cancellationToken = default)
+    {
+        var cursor = await _db.ListCollectionNamesAsync();
+
+        return cursor.ToEnumerable()
+            .Where(x => x.StartsWith(ConnectionNamePrefix))
+            .Select(x => x.Replace(ConnectionNamePrefix, ""))
+            .ToImmutableArray();
+    }
+
+    public Task DeleteAsync(string index, MemoryRecord record, CancellationToken cancellationToken = default)
+    {
+        var collection = GetCollection(index);
+        return collection.DeleteOneAsync(x => x["_id"] == record.Id);
+    }
+
+    public async IAsyncEnumerable<MemoryRecord> GetListAsync(
+        string index,
+        ICollection<MemoryFilter>? filters = null,
+        int limit = 1,
+        bool withEmbeddings = false,
+        [EnumeratorCancellation] CancellationToken cancellationToken = default)
+    {
+        //need to create a search query and execute it
+        var compound = new BsonDocument();
+        var conditions = new BsonArray();
+        foreach (var filter in filters ?? Array.Empty<MemoryFilter>())
+        {
+            var thisFilter = filter.GetFilters();
+            foreach (var (key, value) in thisFilter)
+            {
+                var condition = new BsonDocument();
+                condition["text"] = new BsonDocument
+                {
+                    { "query", value},
+                    { "path", $"tg_{key}" }
+                };
+                conditions.Add(condition);
+            }
+        }
+        compound["must"] = conditions;
+        var pipeline = new BsonDocument[]
+        {
+            new BsonDocument
+            {
+                {
+                    "$search", new BsonDocument
+                    {
+                        { "index", "searchix__ix_default" },
+                        { "compound", compound }
+                    }
+                }
+            },
+        };
+
+        var collection = GetCollection(index);
+
+        var cursor = await collection.AggregateAsync<BsonDocument>(pipeline);
+        foreach (var document in cursor.ToEnumerable())
+        {      
+            yield return FromBsonDocument(document);
+        }
+    }
+
+    private MemoryRecord FromBsonDocument(BsonDocument doc)
+    {
+        var record = new MemoryRecord();
+        record.Id = doc["_id"].AsString;
+        record.Vector = doc["embedding"].AsBsonArray.Select(x => (float)x.AsDouble).ToArray();
+        foreach (var element in doc.Elements)
+        {
+            if (element.Name.StartsWith("pl_"))
+            {
+                var key = element.Name.Replace("pl_", "");
+                record.Payload[key] = element.Value.AsString;
+            }
+            else if (element.Name.StartsWith("tg_"))
+            {
+                var key = element.Name.Replace("tg_", "");
+                record.Tags[key] = element.Value.AsBsonArray.Select(x => (string?) x.AsString).ToList();
+            }
+        }
+        return record;
+    }
+
+    public IAsyncEnumerable<(MemoryRecord, double)> GetSimilarListAsync(string index, string text, ICollection<MemoryFilter>? filters = null, double minRelevance = 0, int limit = 1, bool withEmbeddings = false, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<string> UpsertAsync(string index, MemoryRecord record, CancellationToken cancellationToken = default)
+    {
+        var collection = GetCollection(index);
+        var bsonDocument = new BsonDocument();
+        bsonDocument["_id"] = record.Id;
+        bsonDocument["embedding"] = new BsonArray(record.Vector.Data.Span.ToArray());
+        foreach (var (key, value) in record.Payload)
+        {
+            bsonDocument[$"pl_{key}"] = value?.ToString();
+        }
+
+        foreach (var (key, value) in record.Tags)
+        {
+            bsonDocument[$"tg_{key}"] = new BsonArray(value);
+        }
+
+        collection.InsertOne(bsonDocument);
+        return Task.FromResult(bsonDocument["_id"].AsString);
+    }
+}
---
+++ src/KernelMemory.AtlasMongoDb.Tests/TestKernelMemory.cs
@@ -0,0 +1,118 @@
+using Microsoft.KernelMemory;
+using Microsoft.KernelMemory.MemoryStorage;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace KernelMemory.AtlasMongoDb.Tests;
+
+
+public class Experiments : IDisposable
+{
+    private const string ConnectionString = "";
+
+    public void Dispose()
+    {
+        var client = new MongoClient(ConnectionString);
+        var utils = new AtlasSearchUtils(ConnectionString, "testgm");
+        utils.DeletedIndicesAsync("default").Wait();
+        client.DropDatabase("testgm");
+    }
+
+    [Fact]
+    public async Task GetIndex()
+    {
+        var client = new MongoClient(ConnectionString);
+        var database = client.GetDatabase("testgm");
+
+        var atlasUtils = new AtlasSearchUtils(ConnectionString, "testgm");
+
+        var collectionName = "default";
+        var collectionExists = database
+            .ListCollectionNames()
+            .ToEnumerable()
+            .ToHashSet()
+            .Contains(collectionName);
+
+        if (!collectionExists)
+        {
+            database.CreateCollection(collectionName);
+        }
+
+
+        var collection = database.GetCollection<BsonDocument>("default");
+
+        collection.InsertOne(new BsonDocument("name", "test"));
+        var searchIndex = await atlasUtils.GetIndexInfoAsync(collectionName);
+        Assert.False(searchIndex.Exists);
+        var indexStatus  = await atlasUtils.CreateIndexAsync(collectionName, 725);
+
+        while (indexStatus.Status != "ready")
+        {
+            await Task.Delay(1000);
+            indexStatus = await atlasUtils.GetIndexInfoAsync(collectionName);
+        }
+    }
+
+    [Fact]
+    public async Task Verify_init_index_smoke() 
+    {
+        AtlasMemory sut = new(ConnectionString, "testgm");
+        await sut.CreateIndexAsync("default", 725, CancellationToken.None);
+
+        //verify that the index is created
+        var indexes = await sut.GetIndexesAsync(CancellationToken.None);
+        Assert.Contains("default", indexes);
+
+        var record = new MemoryRecord();
+        record.Id = "TEST";
+        record.Tags = new TagCollection
+        {
+            { "category", new List<string?> { "Fantasy", "ScienceFiction" } },
+            { "owner", new List<string?> { "alkampfer" } }
+        };
+        record.Payload = new Dictionary<string, object>() 
+        {
+            ["citations"] = "bla bla bla bla",
+            ["title"] = "This is a nice test",
+        };
+
+        //generate a random array of 725 element of type float
+        var vector = new float[725];
+        for (int i = 0; i < 725; i++)
+        {
+            vector[i] = (float) new Random().NextDouble();
+        }
+
+        record.Vector = vector;
+
+        await sut.UpsertAsync("default", record, CancellationToken.None);
+
+        var filters = new List<MemoryFilter>();
+        var filter = new MemoryFilter();
+        filter.ByTag("category", "Fantasy");
+        filters.Add(filter);
+
+        var l = new List<MemoryRecord>();
+
+        await Task.Delay(2000);
+
+        await foreach (var item in sut.GetListAsync("default", filters))
+        {
+            l.Add(item);
+        }
+        Assert.Single(l);
+        Assert.Equal("TEST", l[0].Id);
+        Assert.Equal("Fantasy", l[0].Tags["category"][0]);
+
+        await sut.DeleteAsync("default", record, CancellationToken.None);
+
+        l = new List<MemoryRecord>();
+        await foreach (var item in sut.GetListAsync("default", filters))
+        {
+            l.Add(item);
+        }
+        Assert.Empty(l);
+
+        await sut.DeleteIndexAsync("default", CancellationToken.None);
+    }
+}
